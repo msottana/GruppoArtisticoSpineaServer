0e29296bdbce4a2add803684d8379b2a8e4e75c9
Matteo Sottana <s_matteo@tiscali.it>
2015-07-04 10:58:03 +0200

    Cambiato in utf8
    Charset imostato a utf8 in pittori.php

--- pittori.php
+++ pittori.php
@@ -3,7 +3,7 @@
 
     <head>
         
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8">
         <!--
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
