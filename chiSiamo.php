8a4412b56f17882e7e1b49c94c8f803b09f9f35f
Matteo Sottana <s_matteo@tiscali.it>
2015-07-13 16:59:05 +0200

    Aggiunti require per navbar e footer
    Aggiunti i require per i file della navbar e del footer alle pagine in
    oggetto

--- chiSiamo.php
+++ chiSiamo.php
@@ -1,98 +1,62 @@
 <!DOCTYPE html>
 <html lang="it">
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <!--
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <!--
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>Chi siamo - Gruppo Artistico Spinea</title>
+        <title>Chi siamo - Gruppo Artistico Spinea</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/business-casual.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/business-casual.css" rel="stylesheet">
 
-    <!-- Font-Awesome CSS -->
-    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <!-- Font-Awesome CSS -->
+        <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- Nostri Custom CSS -->
-    <link href="css/custom.css" rel="stylesheet">
-    
-    <!-- Fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+        <!-- Nostri Custom CSS -->
+        <link href="css/custom.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- Fonts -->
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
 
-</head>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-<body>
+    </head>
 
-    <div>
-        <a href="index.html">
-            <img class="img-responsive img-full" src="img/logoDefinitivo.jpg" alt="">
-        </a>
-    </div>
+    <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
-                <a class="navbar-brand" href="index.html">Gruppo Artistico Spinea</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="index.html">Home</a>
-                    </li>
-                    <li>
-                        <a href="galleria.html">Galleria</a>
-                    </li>
-                    <li>
-                        <a href="pittori.html">Pittori</a>
-                    </li>
-                    <li>
-                        <a href="eventi.html">Eventi</a>
-                    </li>
-                    <li>
-                        <a href="chiSiamo.html">Chi siamo</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+        <div>
+            <a href="index.html">
+                <img class="img-responsive img-full" src="img/logoDefinitivo.jpg" alt="">
+            </a>
         </div>
-        <!-- /.container -->
-    </nav>
 
-    <div class="container">
+        <!-- Navigation -->
+        <?php require("navbar.php"); ?>
 
-        <?php
-        require_once 'ClassiPHP/EstraiDati.php';
-        $estrai = new EstraiDati();
-        $gruppo = $estrai->estraiContenuto("Gruppo");
-        ?>
+        <div class="container">
+
+            <?php
+            require_once 'ClassiPHP/EstraiDati.php';
+            $estrai = new EstraiDati();
+            $gruppo = $estrai->estraiContenuto("Gruppo");
+            ?>
             <div class="row">
                 <div class="box">
                     <div class="col-lg-12">
@@ -127,7 +91,7 @@
                             <strong> <?php echo utf8_encode($gruppo[0][4]) ?></strong>
                         </p>
                         <p>Email:
-                            <strong><a href="<?php echo "mailto:".($gruppo[0][5]) ?>"><?php echo utf8_encode($gruppo[0][5]) ?></a></strong>
+                            <strong><a href="<?php echo "mailto:" . ($gruppo[0][5]) ?>"><?php echo utf8_encode($gruppo[0][5]) ?></a></strong>
                         </p>
                         <p>Indirizzo:
                             <strong> <?php echo utf8_encode($gruppo[0][3]) ?></strong>
@@ -137,35 +101,16 @@
                 </div>
             </div>
         </div>
-        
-            <footer>
-            <div class="container" id="footer">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-4 text-left">
-                        <p>
-                            <a href="http://www.spinea.gov.it/" target="_blank">Comune di Spinea</a>
-                        </p>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 text-center">
-                        <p>Copyright &copy; Gruppo Artistico Spinea 2015</p>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 text-right">
-                        <p>Seguici su:
-                            <a href="http://facebook.com" target="_blank" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
-                            <a href="http://plus.google.com" target="_blank" class="btn btn-social-icon btn-google-plus"><i class="fa fa-google-plus"></i></a>
-                            <a href="http://twitter.com" target="_blank" class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+        <!-- footer -->
+        <?php require("footer.php"); ?>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
 
-</body>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
 
 </html>
--- pittore.php
+++ pittore.php
@@ -45,43 +45,7 @@
         </div>
 
         <!-- Navigation -->
-        <nav class="navbar navbar-default" role="navigation">
-            <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
-                    <a class="navbar-brand" href="index.html">Gruppo Artistico Spinea</a>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="index.html">Home</a>
-                        </li>
-                        <li>
-                            <a href="galleria.html">Galleria</a>
-                        </li>
-                        <li>
-                            <a href="pittori.html">Pittori</a>
-                        </li>
-                        <li>
-                            <a href="eventi.html">Eventi</a>
-                        </li>
-                        <li>
-                            <a href="chiSiamo.html">Chi siamo</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-            <!-- /.container -->
-        </nav>
+        <?php require("navbar.php"); ?>
 
         <div class="container">
             <?php
@@ -162,27 +126,8 @@
         </div>
         <!-- /.container -->
 
-        <footer>
-            <div class="container" id="footer">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-4 text-left">
-                        <p>
-                            <a href="http://www.spinea.gov.it/" target="_blank">Comune di Spinea</a>
-                        </p>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 text-center">
-                        <p>Copyright &copy; Gruppo Artistico Spinea 2015</p>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 text-right">
-                        <p>Seguici su:
-                            <a href="http://facebook.com" target="_blank" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
-                            <a href="http://plus.google.com" target="_blank" class="btn btn-social-icon btn-google-plus"><i class="fa fa-google-plus"></i></a>
-                            <a href="http://twitter.com" target="_blank" class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <!-- footer -->
+        <?php require("footer.php"); ?>
 
         <!-- jQuery -->
         <script src="js/jquery.js"></script>
