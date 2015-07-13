e18c09b3908476520aee6b5b6cb1d2b50bc51196
Matteo Sottana <s_matteo@tiscali.it>
2015-07-13 16:42:41 +0200

    Aggiunto footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="container" id="footer">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 text-left">
+                <p>
+                    <a href="http://www.spinea.gov.it/" target="_blank">Comune di Spinea</a>
+                </p>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 text-center">
+                <p>Copyright &copy; Gruppo Artistico Spinea 2015</p>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 text-right">
+                <p>Seguici su:
+                    <a href="http://facebook.com" target="_blank" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+                    <a href="http://plus.google.com" target="_blank" class="btn btn-social-icon btn-google-plus"><i class="fa fa-google-plus"></i></a>
+                    <a href="http://twitter.com" target="_blank" class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
